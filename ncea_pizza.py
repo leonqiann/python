9388afa476e8c2254431f2062ece2bf3496469b0
leonqiann <leonqian@riccarton.school.nz>
2025-03-18 09:21:48 +1300

    pizza

---
+++ ncea_pizza.py
@@ -0,0 +1,13 @@
+#ts prgram will ask how many pizzas and print the amoutn of pizaas
+
+#list of pizza options (storage)
+pizzalist = []
+
+# ask how many of each piza (storage)
+cheese = int(input("How many cheese pizzas do we want?"))
+chicken = int(input("How many chicken pizzas do we want?"))
+pepperoni = int(input("How many pepperoni pizzas do we want?"))
+veggie = int(input("How many veggie pizzas do we want?"))
+# go thru list of pizzas (repetition)
+
+#exclude orders of 0 pizzas (selection)
