098d1acbb298caaf06af77ed7e48e6538f575a56
leonqiann <leonqian@riccarton.school.nz>
2025-03-18 09:29:56 +1300

    pizza2

--- ncea_pizza.py
+++ ncea_pizza.py
@@ -2,12 +2,53 @@
 
 #list of pizza options (storage)
 pizzalist = []
-
+stop = 0 
 # ask how many of each piza (storage)
-cheese = int(input("How many cheese pizzas do we want?"))
-chicken = int(input("How many chicken pizzas do we want?"))
-pepperoni = int(input("How many pepperoni pizzas do we want?"))
-veggie = int(input("How many veggie pizzas do we want?"))
+while True:
+    try:
+        cheese = int(input("How many cheese pizzas do we want?"))
+        if cheese == stop:
+            break
+        if cheese > stop:
+            pizzalist.append(cheese)
+            break
+    except ValueError:
+        print('Please enter a valid amount')
+
+while True:
+    try:
+        chicken = int(input("How many chicken pizzas do we want?"))
+        if cheese == stop:
+            break
+        if cheese > stop:
+            pizzalist.append(cheese)
+            break
+    except ValueError:
+        print('Please enter a valid amount')
+
+while True:
+    try:
+        pepperoni = int(input("How many pepperoni pizzas do we want?"))
+        if cheese == stop:
+            break
+        if cheese > stop:
+            pizzalist.append(cheese)
+            break
+    except ValueError:
+        print('Please enter a valid amount')
+
+while True:
+    try:
+        veggie = int(input("How many veggie pizzas do we want?"))
+        if cheese == stop:
+            break
+        if cheese > stop:
+            pizzalist.append(cheese)
+            break
+    except ValueError:
+        print('Please enter a valid amount')
+
+
 # go thru list of pizzas (repetition)
 
 #exclude orders of 0 pizzas (selection)
